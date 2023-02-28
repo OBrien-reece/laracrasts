a718b88fa7bb041654e50476d5a9bf054557aae6
OBrien-reece <indeche.evans.o@gmail.com>
2023-02-28 23:11:17 +0300

    designed post comment form

--- blog/resources/views/components/post/post-comment.blade.php
+++ blog/resources/views/components/post/post-comment.blade.php
@@ -1,16 +1,19 @@
 @props(['comment'])
 
-<article class="flex bg-gray-100 border space-x-4 border-gray-200 p-6 rounded-xl">
-    <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60px" height="60px" class="rounded-xl">
-    </div>
+<x-panel class="bg-gray-50">
+    <article class="flex bg-gray-100 space-x-4">
+        <div class="flex-shrink-0">
+            <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60px" height="60px" class="rounded-xl">
+        </div>
 
-    <div>
-        <header class="mb-4">
-            <h3 class="font-bold">{{ $comment->author->username }}</h3>
-            <p class="text-xs">Posted<time> {{ $comment->created_at->diffForHumans() }}</time></p>
-        </header>
-        <p>{{ $comment->body }}</p>
-    </div>
+        <div>
+            <header class="mb-4">
+                <h3 class="font-bold">{{ $comment->author->username }}</h3>
+                <p class="text-xs">Posted<time> {{ $comment->created_at->diffForHumans() }}</time></p>
+            </header>
+            <p>{{ $comment->body }}</p>
+        </div>
 
-</article>
+    </article>
+</x-panel>
+
