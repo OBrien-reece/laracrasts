967ff9113a2b51541d5d1bc064b76120914043ef
OBrien-reece <indeche.evans.o@gmail.com>
2023-03-02 00:26:11 +0300

    corrected login/register error

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -39,4 +39,4 @@ Route::post('/admin/posts/', [PostController::class, 'store'])->middleware('admi
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
